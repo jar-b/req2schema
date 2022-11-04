3c6e213245e88a04547ab0fcf97cb5617c8adb2e
Jared Baker <jared.baker@hashicorp.com>
2022-11-04 11:53:57 -0400

    add optional output file flag

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
+var out string
+
+func init() {
+	// slightly better usage output
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [flags] [filename]\n\nFlags:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	if len(os.Args) != 2 {
+	flag.StringVar(&out, "out", "", "output file (optional, prints to stdout if omitted)")
+	flag.Parse()
+
+	if flag.NArg() != 1 {
 		log.Fatal("unexpected number of args")
 	}
-	b, err := os.ReadFile(os.Args[1])
+	in := flag.Arg(0)
+
+	b, err := os.ReadFile(in)
 	if err != nil {
 		log.Fatal(fmt.Errorf("reading input: %w", err))
 	}
 
-	out, err := requestToSchema(b)
+	schm, err := requestToSchema(b)
 	if err != nil {
 		log.Fatal(fmt.Errorf("schema generation: %w", err))
 	}
-	fmt.Println(string(out))
+
+	if out != "" {
+		fmt.Printf("writing to: %s\n", out)
+		os.WriteFile(out, schm, 0644)
+	} else {
+		fmt.Println(string(schm))
+	}
 }
